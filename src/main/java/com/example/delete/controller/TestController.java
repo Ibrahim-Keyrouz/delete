4d7451dce6fbc19151e98ea5f9f08ec25db9de85
bk <bob.keyrouz@gmail.com>
2023-06-07 01:22:52 +0300

    ht

--- src/main/java/com/example/delete/controller/TestController.java
+++ src/main/java/com/example/delete/controller/TestController.java
@@ -1,16 +1,28 @@
 package com.example.delete.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 @RestController
 @RequestMapping("/test")
+@RefreshScope
 public class TestController {
-
+    @Value("${message:Hello default}")
+    private String message;
     @GetMapping("/get")
+
     public String returnString() {
         return "hello";
+    }
+
+
+
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
     }
 
 
